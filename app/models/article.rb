fab5a69cff0e8471c43e714d912201395cdfdee3
julien mamalian <julien.mamalian@gmail.com>
2015-07-21 23:25:39 +0200

    remove issue solving

--- app/models/article.rb
+++ app/models/article.rb
@@ -25,9 +25,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" },
-    :storage => :s3,
-    :bucket  => ENV['journal-yann-production']
+    styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
